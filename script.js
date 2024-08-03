9039f40c35306ec6769ac29df7cff809da56bd46
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-03 23:25:53 +0530

    frontend

---
+++ script.js
@@ -0,0 +1,37 @@
+
+//cursor
+var crsr = document.querySelector("#cursor")
+var crsrblur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    crsrblur.style.left = dets.x-100+"px"
+    crsrblur.style.top = dets.y-100+"px"
+})
+
+
+gsap.to("#nav",{
+    backgroundColor:"#0000",
+    height:"120px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+});
+
+gsap.to('#main',{
+    backgroundColor : "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -30vh",
+        end:"top -80vh",
+        scrub : 2
+    }
+})
